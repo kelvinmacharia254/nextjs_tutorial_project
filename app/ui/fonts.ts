f51c87375c56aab64e4984cfabfb52334b778c9c
kelvinmacharia254 <pilotkelvin0@gmail.com>
2024-12-11 21:12:27 +0300

    initial settings and adding optimized fonts and images in nextjs

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+// add a custom font to your project
+// 
+import { Inter, Lusitana  } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700'], 
+  });
